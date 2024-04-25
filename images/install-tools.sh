b60f17ea83ba6567493fc4be20eefb8c6da7621a
cyclinder <qifeng.guo@daocloud.io>
2024-04-25 18:01:18 +0800

    add Dockerfile and install.sh

--- images/install-tools.sh
+++ images/install-tools.sh
@@ -3,19 +3,18 @@
 set -x
 set -e
 
-mkdir -p /host/usr/bin
-
+mkdir -p /host/usr/bin && mkdir -p /host/usr/lib
 OS=$(grep -E '^ID=(.*)' /etc/os-release | awk -F '=' '{print $2}')
 case "${OS}" in
     ubuntu)
         apt-get update
         apt-get install -y kmod
         apt-get install -y smc-tools
-        mkdir /host/usr/lib && cp /usr/lib/libsmc-preload.so /host/usr/lib
+        cp /usr/lib/libsmc-preload.so /host/usr/lib
         ;;
     centos,fedora)
         yum install -y smc-tools
-        mkdir /host/usr/lib64 && cp /usr/lib64/libsmc-preload.so /host/usr/lib64
+        cp /usr/lib64/libsmc-preload.so /host/usr/lib
         ;;
 esac
 
